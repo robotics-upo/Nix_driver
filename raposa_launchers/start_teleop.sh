09b57b3fa1f1e4434b141c9819f37e882a534c12
Rafael Rey <reyarcenegui@gmail.com>
2019-10-15 15:20:22 +0200

    added navigation pc connection check

--- raposa_launchers/start_teleop.sh
+++ raposa_launchers/start_teleop.sh
@@ -1,12 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sleep 1
+sleep 10
+
 source /opt/ros/kinetic/setup.bash
 source /home/raposa/catkin_raposa/devel/setup.bash
 
-export ROS_MASTER_URI=http://localhost:11311
 export ROS_HOSTNAME="raposa-idmind"
 
-sleep 5
 
-roslaunch raposa_launchers teleop.launch
+if ping -q -c5 raposa-nav &> /dev/null
+then
+	export ROS_MASTER_URI=http://raposa-nav:11311
+	export ROS_IP=10.5.5.2
+	roslaunch raposa_launchers teleop.launch
+	echo "true, found roscore"
+else
+	export ROS_MASTER_URI=http://localhost:11311
+	roscore
+	roslaunch raposa_launchers teleop.launch
+	echo "roscore not found, starting own one"
+fi
