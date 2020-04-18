eedbeb08461c3849439b2a6427daf85df416230f
Rafael Rey <reyarcenegui@gmail.com>
2020-04-18 15:21:57 +0200

    added raposa scripts for starting up with systemd service

---
+++ raposa_launchers/scripts/start_raposa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#TODO Maybe the best is to publish only /odom  (not tf) when there is navigation pc, and this last one will decide if publish or not odometry    
+#TODO   So if there is no navigation pc, the drivers pc should publish /odom and the corresponding tf odom->base_link
+#TODO   This can be passed as an arg to the launch and the launch pass it to the odometry node
+
+source /opt/ros/kinetic/setup.bash
+source /home/raposa/catkin_raposa/devel/setup.bash
+export ROS_HOSTNAME=raposa-idmind
+#Check if we have ping to the raposa-nav host:
+
+ping -c1 raposa-nav
+if [ $? -eq 0 ]
+then # If there is ping, wait for ros master
+    export ROS_MASTER_URI=http://raposa-nav:11311
+    until rostopic list ; do echo "Waiting for roscore..."; sleep 1; done
+    roslaunch raposa_launchers teleop.launch
+else #If there is no ping, start the robot in basic mode (only drivers and roscore in the bottom pc)
+    export ROS_MASTER_URI=http://raposa-idmind:11311
+    roslaunch raposa_launchers teleop.launch 
+fi
+
