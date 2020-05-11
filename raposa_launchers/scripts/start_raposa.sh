f8ac389ba6980b8e39bd8538804ef77ac3ee7de0
Rafael Rey <reyarcenegui@gmail.com>
2020-05-11 17:55:47 +0200

    working and tested

--- raposa_launchers/scripts/start_raposa.sh
+++ raposa_launchers/scripts/start_raposa.sh
@@ -6,6 +6,8 @@
 source /opt/ros/kinetic/setup.bash
 source /home/raposa/catkin_raposa/devel/setup.bash
 export ROS_HOSTNAME=raposa-idmind
+export ROS_IP=192.168.8.3
+rosclean purge -y
 #Check if we have ping to the raposa-nav host:
 
 ping -c1 raposa-nav
@@ -13,9 +15,13 @@ if [ $? -eq 0 ]
 then # If there is ping, wait for ros master
     export ROS_MASTER_URI=http://raposa-nav:11311
     until rostopic list ; do echo "Waiting for roscore..."; sleep 1; done
+    sudo rm /home/raposa/.bashrc
+    sudo cp /home/raposa/.bashrc_nav /home/raposa/.bashrc
     roslaunch raposa_launchers teleop.launch
 else #If there is no ping, start the robot in basic mode (only drivers and roscore in the bottom pc)
     export ROS_MASTER_URI=http://raposa-idmind:11311
+    sudo rm  /home/raposa/.bashrc
+    sudo cp /home/raposa/.bashrc_isolated /home/raposa/.bashrc
     roslaunch raposa_launchers teleop.launch only_driver_pc:=true
 fi
 
